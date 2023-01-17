a9940d918fb6b74e4c0d05fd4180c1647f14f7ed
brytton <brytton@katharsis>
2023-01-16 18:43:18 -0800

    added remap for going up and down

--- lua/katharsis/remap.lua
+++ lua/katharsis/remap.lua
@@ -9,6 +9,8 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 -- For automatically centering the screen
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-j>", "<C-d>zz")
+vim.keymap.set("n", "<C-k>", "<C-u>zz")
 vim.keymap.set("n", "n", "nzzzv")
 vim.keymap.set("n", "N", "Nzzzv")
 
